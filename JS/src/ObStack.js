df2a2f6c4dc8b7231f0d2a67257784e777ac4a21
Thomas Lee <Thomas.e.lee7@gmail.com>
2017-09-29 14:18:56 -0500

    Create ObStack.js

---
+++ JS/src/ObStack.js
@@ -0,0 +1,18 @@
+//extends ArrayList<T> ??
+var ObStack = class ObStack {
+
+  ObStack.serialVersionUID = 1L;
+
+  function pop() {
+    //returns top item in object stack
+    //removes it
+  }
+
+  function push(O) {
+    //adds objec O to stack
+  }
+
+  function peek() {
+    //returns item on top of stack, just to view
+  }
+}
