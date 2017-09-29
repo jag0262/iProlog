5ad25111c82c112f94712c50f45be59545c73a97
Thomas Lee <Thomas.e.lee7@gmail.com>
2017-09-29 12:11:26 -0500

    Create IMap.js

---
+++ JS/src/IMap.js
@@ -0,0 +1,64 @@
+var IMap = class IMap{
+  IMap.serialVersionUID = 1L;
+  //add Hashmap
+  
+  function IMap(){
+    
+  }
+  
+  function clear(){
+  
+  }
+  
+  function put(key, val){
+    //return bool
+  }
+  
+  function get(key){
+    //return IntMap
+  }
+  
+  function remove(key, val){
+    //return bool
+  }
+  
+  function remove(key){
+    //return bool
+  }
+  
+  function size(){
+    //return int
+  }
+  
+  function keyIteratro(){
+    //return iterator
+  }
+  
+  function toString(){
+  
+  }
+  
+  function create(l){
+    //return IMap(integers)
+  }
+  
+  function put(imaps, pos, key, val){
+    //return bool
+  }
+  
+   function get(iMaps, vmaps, keys) {
+    //return array of ints
+   }
+   
+   function show(imaps){
+    //return string
+   }
+   
+   function show(is){
+     //return string
+   }
+
+
+  
+  
+}
