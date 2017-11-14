27eb31cdbc9ad886bd76859d15d497a8473eeb3f
Thomas Lee <Thomas.e.lee7@gmail.com>
2017-11-14 00:30:18 -0600

    Update clause.test.js

---
+++ JS/src/test/clause.test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var expect = require("chai").expect;
+var cla = require("../Clause.js");
+
+describe("Clause", function(){
+    it("holds a prolog clause", function(){
+        //fake news
+        assert.equal(-1, [1,2,3].indexOf(4));
+    });
+    
+});
